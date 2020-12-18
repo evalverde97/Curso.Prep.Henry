4eb97d86e9ce9df0a07506d035cf4bdff5c6604d
evalverde97 <ezequielvalverde97@gmail.com>
2020-12-18 18:19:06 -0300

    all test passed

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -96,14 +96,14 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-if (num / 2 == 0) {return true;} return false;
+if (num % 2 === 0) {return true;} return false;
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if ( num / 2 == 1) {return true;} return false;
+  if ( num % 2 === 1) {return true;} return false;
 }
 
 function elevarAlCuadrado(num) {
